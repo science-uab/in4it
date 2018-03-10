7d697419c82a425b14f59ffcb0d13dfe349d9b1b
mihaioltean <mihai.oltean@gmail.com>
2018-03-10 11:53:29 +0200

    combinari

---
+++ src/combinari.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <time.h>
+
+//--------------------------------------
+void afiseaza_combinare(int m, int *solutie)
+{
+	for (int j = 0; j < m; j++)
+		printf("%d ", solutie[j]);
+	printf("\n");
+}
+//--------------------------------------
+void genereaza_combinari(int n, int m, int *solutie, int pozitie_curenta)
+{
+	if (pozitie_curenta == m)
+		afiseaza_combinare(m, solutie)
+		;
+	else {
+		for (int valoare = pozitie_curenta == 0?1 : solutie[pozitie_curenta - 1] + 1; valoare <= n; valoare++) {
+				solutie[pozitie_curenta] = valoare;
+				genereaza_combinari(n, m, solutie, pozitie_curenta + 1);
+			}
+		}
+}
+//--------------------------------------
+int main(void)
+{
+	int n, m;
+	printf("n, m = ");
+	scanf("%d%d", &n, &m);
+
+	int *solutie;
+	solutie = new int[m];
+
+	clock_t start_time = clock();
+	genereaza_combinari(n, m, solutie, 0);
+	clock_t stop_time = clock();
+
+	printf("timp de rulare = %lf\n", (stop_time - start_time) / (double)CLOCKS_PER_SEC);
+	delete[] solutie;
+
+	getchar();
+	getchar();
+	return 0;
+}
